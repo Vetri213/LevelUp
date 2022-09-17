389aa4212358b90b1a1f10bb2c253d869bec54a0
Vetri <vetrivelbalaji333@gmail.com>
2022-09-17 11:28:17 -0400

    fixed main screen

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def play():
 
         SCREEN.fill("black")
 
-        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True,
+        PLAY_TEXT = get_font(45).render("SELECT AN EXERCISE", True,
                                         "White")
         PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
@@ -28,9 +28,27 @@ def play():
         PLAY_BACK = Button(image=None, pos=(640, 460),
                            text_input="BACK", font=get_font(75),
                            base_color="White", hovering_color="Green")
+        PUSH_UPS = Button(image=None, pos=(220, 380),
+                           text_input="PUSH UPS", font=get_font(50),
+                           base_color="White", hovering_color="Green")
+        SIT_UPS = Button(image=None, pos=(660, 380),
+                           text_input="SIT UPS", font=get_font(50),
+                           base_color="White", hovering_color="Green")
+        SQUATS = Button(image=None, pos=(1060, 380),
+                           text_input="SQUATS", font=get_font(50),
+                           base_color="White", hovering_color="Green")
 
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(SCREEN)
+
+        PUSH_UPS.changeColor(PLAY_MOUSE_POS)
+        PUSH_UPS.update(SCREEN)
+
+        SIT_UPS.changeColor(PLAY_MOUSE_POS)
+        SIT_UPS.update(SCREEN)
+
+        SQUATS.changeColor(PLAY_MOUSE_POS)
+        SQUATS.update(SCREEN)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -39,12 +57,17 @@ def play():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
                     main_menu()
+                if PUSH_UPS.checkForInput(PLAY_MOUSE_POS):
+                    import pushup
+                    pushup.main()
+                    pygame.quit()
+                if SIT_UPS.checkForInput(PLAY_MOUSE_POS):
+                    import situp
+                    situp.main()
+                    pygame.quit()
+                ## ONE MORE
 
         pygame.display.update()
-    # connect it to pushup.py
-    # import pushup
-    # pushup.main()
-    # pygame.quit()
 
 
 def options():
@@ -82,12 +105,12 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#006400")
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
         PLAY_BUTTON = Button(image=pygame.image.load("Assets/Play Rect.png"),
                              pos=(640, 250),
-                             text_input="Push Ups", font=get_font(75),
+                             text_input="PLAY", font=get_font(75),
                              base_color="#d7fcd4", hovering_color="White")
         OPTIONS_BUTTON = Button(
             image=pygame.image.load("Assets/Options Rect.png"), pos=(640, 400),
@@ -116,6 +139,7 @@ def main_menu():
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
+
 
         pygame.display.update()
 
